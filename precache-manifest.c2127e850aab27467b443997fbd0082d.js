a047b6fe59fa71d197c9d35a2a01367c45d749bd
min geyoung min <alsrudals2013@gmail.com>
2019-12-29 19:09:35 +0900

    Updates

---
+++ precache-manifest.c2127e850aab27467b443997fbd0082d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d45c60bd605b2673091902bbc23da5f0",
+    "url": "/my-app-2019/index.html"
+  },
+  {
+    "revision": "2422e76a52111f9c575f",
+    "url": "/my-app-2019/static/css/main.4be38c43.chunk.css"
+  },
+  {
+    "revision": "c943760f3adb0d625ccc",
+    "url": "/my-app-2019/static/js/2.f9fad1b0.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/my-app-2019/static/js/2.f9fad1b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2422e76a52111f9c575f",
+    "url": "/my-app-2019/static/js/main.f3f08299.chunk.js"
+  },
+  {
+    "revision": "5fc7396acc61574fd875",
+    "url": "/my-app-2019/static/js/runtime-main.c7868fac.js"
+  }
+]);
